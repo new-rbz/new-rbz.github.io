11dbb276b062b344a37396ed2400ed79a66d6736
Robert Beasy <robert.beasy@gmail.com>
2016-07-09 10:27:49 -0500

    Revert "ng2 experiment"
    This reverts commit f5366f7bd5cfc5f77f474788d216ab90bc3becfb.

--- dashboard/e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class DashboardPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('dashboard-app h1')).getText();
-  }
-}
--- dashboard/src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {DashboardAppComponent} from './dashboard.component';
