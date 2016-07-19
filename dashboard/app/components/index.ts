7c41cee44ad0c7297077682534a39dbd19699537
Robert Beasy <robert.beasy@gmail.com>
2016-07-18 21:42:55 -0500

    o

--- dashboard/app/components/index.ts
+++ dashboard/app/components/index.ts
@@ -1,4 +1,12 @@
-export { Todo } from './todo/todo';
 export { About } from './about/about';
+export { FormComponent } from './forms/form';
 export { Profile } from './profile/profile';
 export { TestComponent } from './test/test';
+
+export { AutoGrowDirective } from './test/auto-grow.directive';
+export { GoogleSheetsService } from './test/googleSheetsService';
+export { ListType } from './test/ListType';
+export { YahooDataService } from './test/yahooDataService';
+
+export { Todo } from './todo/todo';
+export { TodoItem } from './todo/models/todoItem';
